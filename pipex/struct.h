4d26bb7712d4351ee5a7f63ba85bfe7bedc1a68f
Julien Cros <julien.cros86@gmail.com>
2023-03-28 14:23:45 +0200

    starting parsing

--- pipex/struct.h
+++ pipex/struct.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juliencros <juliencros@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/27 16:27:59 by juliencros        #+#    #+#             */
-/*   Updated: 2023/03/27 17:30:43 by juliencros       ###   ########.fr       */
+/*   Created: 2023/03/27 16:14:33 by juliencros        #+#    #+#             */
+/*   Updated: 2023/03/27 16:24:07 by juliencros       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,43 +17,14 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
-size_t	ft_strlen(const char *s);
+#ifndef STRUCT_H
+#define STRUCT_H
 
-int ft_strchr(char *s, char *base)
+typedef struct s_args
 {
-	int i;
-	int j;
+	char *file;
+	char **argv;
+	int fds;
+}	t_args;
 
-	i = 0;
-	j = 0;
-	if (!s || !base)
-		return (1);
-	while (i < ft_strlen(base))
-	{
-		j = 0;
-		if (base[i] == s[j])
-		{
-			while (base[i] == s[j])
-			{
-				i++;
-				j++;
-			}
-		if (j == ft_strlen(s))
-			return (0);
-		}
-		printf("%d\n", j);
-		i++;
-	}
-	return (1);
-}
-
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-	
-	i = 0;
-	while (i[s])
-		++i;
-	return (i);
-}
+#endif
---
+++ pipex/utils.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliencros <juliencros@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 16:41:44 by juliencros        #+#    #+#             */
+/*   Updated: 2023/03/27 16:43:36 by juliencros       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+int ft_strchr(char *s, char *base);
+
+#endif
