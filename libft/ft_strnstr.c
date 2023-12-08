5a54d90834d90b6ca6011c1e3c46fd98de8b6290
Julien Cros <julien.cros86@gmail.com>
2023-12-08 10:05:30 +0100

    chore: norm libft

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcros <jcros@student.42.fr>                +#+  +:+       +#+        */
+/*   By: juliencros <juliencros@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 10:24:50 by jcros             #+#    #+#             */
-/*   Updated: 2022/11/14 10:09:20 by jcros            ###   ########.fr       */
+/*   Updated: 2023/12/08 10:05:05 by juliencros       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,16 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
-	// size_t	j;
 
 	i = 0;
-	// j = 0;
-	// if (!*little)
-	// 	return((char *)big);
-	// while (big[i] && i < len)
-	// {
-	// 	j = 0;
-	// 	if (little[i] == big[j])
-	// 	{
-	// 		while (big[j + i] && big[j + i] == little[j] && i < len - i - 1)
-	// 			j++;
-	// 		if (j == len )
-	// 			return ((char *)big);
-	// 	}
-	// 	i++;
-	// }
-	// return (NULL);
-
 	if (!*little)
 		return ((char *)big);
 	if (!len)
 		return (0);
 	while (big[i] && len--)
 	{
-		if (ft_strncmp(little, big + i, ft_strlen(little)) == 0 && ft_strlen(little) <= len + 1)
+		if (ft_strncmp(little, big + i, ft_strlen(little)) == 0
+			&& ft_strlen(little) <= len + 1)
 			return ((char *)big + i);
 		i++;
 	}
