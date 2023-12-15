b6d359961af02c98015e9eb34f6b07d8ae66b475
Julien Cros <julien.cros86@gmail.com>
2023-12-15 15:43:56 +0100

    feat: ex00

---
+++ cpp/cpp-03/ex00/ClapTrap.hpp
@@ -0,0 +1,22 @@
+#ifndef ClapTrap_HPP
+#define ClapTrap_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string name;
+		int hitPoints;
+		int energyPoints; 
+		int attackDamage; 
+
+	public:
+		explicit ClapTrap(std::string name);
+		~ClapTrap();
+		void attack(std::string const &target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ cpp/cpp-03/ex00/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap clapTrap("ClapTrap");
+
+	clapTrap.attack("target");
+	clapTrap.takeDamage(5);
+	clapTrap.beRepaired(5);
+	return (0);
+}
