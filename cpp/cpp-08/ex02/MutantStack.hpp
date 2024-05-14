da485fe8f97e232f8ed70b42c5bc72c2ceb6a672
Julien Cros <julien.cros86@gmail.com>
2024-05-14 12:23:01 +0200

    feat: cpp08/ex02 done

---
+++ cpp/cpp-08/ex02/MutantStack.hpp
@@ -0,0 +1,36 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <iterator>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+	MutantStack() : std::stack<T>(){};
+	~MutantStack(){};
+	MutantStack(const MutantStack &copy) : std::stack<T>() { *this = copy; };
+
+	void operator=(const MutantStack &rhs)
+	{
+		std::stack<T>::operator=();
+		{
+			return *this;
+		};
+	};
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+
+	iterator begin()
+	{
+		return (this->c.begin());
+	}
+	iterator end()
+	{
+		return (this->c.end());
+	}
+};
+
+#endif
---
+++ cpp/cpp-08/ex02/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "MutantStack.hpp"
+
+int main()
+{
+	MutantStack<int> stack;
+
+	stack.push(10);
+	stack.push(5);
+	stack.push(3);
+	stack.push(7);
+	stack.push(4);
+	std::cout << "Top number: " << stack.top() << std::endl;
+
+	stack.pop();
+
+	std::cout << "Stack size: " << stack.size() << std::endl;
+
+	MutantStack<int>::iterator itBegin = stack.begin();
+	// MutantStack<int>::iterator itEnd = stack.end();
+
+	for (int i = 0; i <= stack.size(); i++)
+		std::cout << *itBegin++ << std::endl;
+}
