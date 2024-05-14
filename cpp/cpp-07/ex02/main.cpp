d4c9468257d408075dbda8c8093ea60f7f0d7784
Julien CROS <jcros@made-f0Br2s2.clusters.42paris.fr>
2024-05-14 12:43:37 +0200

    fix: catch error ex02

--- cpp/cpp-07/ex02/main.cpp
+++ cpp/cpp-07/ex02/main.cpp
@@ -3,10 +3,19 @@
 
 int main()
 {
-	Array<int> intCalss(10);
-	std::cout << "the size is : " << intCalss.size() << std::endl;
+	try
+	{
+		Array<int> intCalss(10);
+		std::cout << "the size is : " << intCalss.size() << std::endl;
 
-	intCalss[5] = 3;
-	std::cout << "intClass[5] : " << intCalss[5] << std::endl;
-	return (0);
+		intCalss[5] = 3;
+		intCalss[6] = 9;
+		std::cout << "intClass[5] : " << intCalss[5] << std::endl;
+		std::cout << "intClass[6] : " << intCalss[6] << std::endl;
+		return (0);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception caught: " << e.what() << std::endl;
+	}
 }
