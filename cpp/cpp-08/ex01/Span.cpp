58522bf8b3c1626b95fda85c38367eb37c085587
Julien Cros <julien.cros86@gmail.com>
2024-05-14 09:19:33 +0200

    feat: ex01 done

---
+++ cpp/cpp-08/ex01/Span.cpp
@@ -0,0 +1,64 @@
+#include "Span.hpp"
+#include <deque>
+
+Span::Span() : N(0)
+{
+}
+
+Span::~Span()
+{
+}
+
+Span::Span(unsigned int N) : N(N)
+{
+}
+
+Span::Span(const Span &copy) : N(copy.N), _deque(copy._deque)
+{
+}
+
+Span &Span::operator=(const Span &rhs)
+{
+	this->N = rhs.N;
+	this->_deque = rhs._deque;
+
+	return *this;
+}
+
+void Span::addNumber(int value)
+{
+	if (_deque.size() == N)
+		throw MaxSizeException();
+
+	_deque.push_back(value);
+}
+
+int Span::shortestSpan()
+{
+	if (_deque.size() < 2)
+		throw TooLittleException();
+
+	std::deque<int>
+			copy = _deque;
+	std::sort(copy.begin(), copy.end());
+
+	int between = copy[1] - copy[0];
+	for (size_t i = 0; i < copy.size() - 1; i++)
+	{
+		// std::cout << (copy[i] - copy[i - 1]) << "  ";
+		if ((copy[i + 1] - copy[i]) < between)
+			between = copy[i + 1] - copy[i];
+	}
+	return (between);
+}
+
+int Span::longestSpan()
+{
+	if (_deque.size() < 2)
+		throw TooLittleException();
+
+	std::deque<int>
+			copy = _deque;
+	std::sort(copy.begin(), copy.end());
+	return (copy[copy.size() - 1] - copy[0]);
+}
---
+++ cpp/cpp-08/ex01/Span.hpp
@@ -0,0 +1,38 @@
+#ifndef SPAN_HPP
+#define SPAN_HPP
+
+#include <iostream>
+#include <deque>
+
+class Span
+{
+private:
+	unsigned int N;
+	std::deque<int> _deque;
+
+public:
+	Span();
+	~Span();
+	Span(unsigned int _size);
+	Span(const Span &copy);
+
+	Span &operator=(const Span &rhs);
+
+	void addNumber(int value);
+	int shortestSpan();
+	int longestSpan();
+
+	class MaxSizeException : public std::exception
+	{
+	public:
+		const char *what() const throw() { return "Reached limit"; };
+	};
+
+	class TooLittleException : public std::exception
+	{
+	public:
+		const char *what() const throw() { return "There id not enough numbers"; };
+	};
+};
+
+#endif
