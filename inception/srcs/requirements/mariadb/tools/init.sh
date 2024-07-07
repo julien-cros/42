548b5d98bab456b43e6e742110af9d67171be9e3
julien <julien@debian.jcros>
2024-07-07 14:23:03 -0400

    fix: Makefile + check if database exist

--- inception/srcs/requirements/mariadb/tools/init.sh
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -7,14 +7,22 @@ while ! mysqladmin ping --silent; do
     sleep 1
 done
 
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" << EOF
-CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE"]
+then
+    echo "Database already exist"
+
+else 
+    echo "creating datase" 
+    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" << EOF
+    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+    CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
 EOF
+fi
 
 mysqladmin shutdown
+
 
 exec "$@"
 
--- inception/srcs/requirements/wordpress/tools/init.sh
+++ inception/srcs/requirements/wordpress/tools/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -f "./wp_config.php" ]
+if [ -f "var/www/html/wp_config.php" ]
 then
 	echo "wordpress aleready installed"
 
@@ -10,7 +10,7 @@ else
 	mv wp-cli.phar /usr/local/bin/wp
 
 	wp core download --allow-root
-	wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOSTNAME" --allow-root
+	wp config create --path="/var/www/html" --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOSTNAME" --allow-root
 	wp core install --url="$WP_SITE_URL" --title=Inception --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
 	wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
 fi
